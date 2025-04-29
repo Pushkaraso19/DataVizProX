421b30a70d1af739fa53b21b31651180b0e35356
Pushkar Asodekar <138507063+Pushkaraso19@users.noreply.github.com>
2025-04-29 22:24:25 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/DataViz-Pro/', // 🔁 replace with your repo name
   plugins: [react()],
-})
+});
